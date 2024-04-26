f805a3c5ae2e0321c533e5a2dc0fc473bdc7d4f2
Jewei Mak <jewei.mak@gmail.com>
2024-04-27 00:33:57 +0800

    Update Rector configuration and add type coverage level

--- rector.php
+++ rector.php
@@ -7,7 +7,6 @@ use Rector\CodingStyle\Rector\Closure\StaticClosureRector;
 use Rector\Config\RectorConfig;
 use Rector\Php55\Rector\String_\StringClassNameToClassConstantRector;
 use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
-use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
 use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
@@ -25,11 +24,12 @@ return RectorConfig::configure()
     ->withPreparedSets(
         codeQuality: true,
         codingStyle: true,
-        typeDeclarations: true,
         privatization: true,
         earlyReturn: true,
     )
     ->withDeadCodeLevel(42)
+    ->withTypeCoverageLevel(37)
+    // ->withImportNames()
     ->withSkip([
         StringClassNameToClassConstantRector::class,
         DisallowedEmptyRuleFixerRector::class,
@@ -37,8 +37,13 @@ return RectorConfig::configure()
         StaticClosureRector::class,
     ])
     ->withRules([
-        AddVoidReturnTypeWhereNoReturnRector::class,
+        //
     ])
     ->withSets([
         LaravelSetList::LARAVEL_110,
+        LaravelSetList::ARRAY_STR_FUNCTIONS_TO_STATIC_CALL,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
+        LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
+        LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+        LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
     ]);
