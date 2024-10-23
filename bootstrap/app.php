5f50223fce97d3092c317f410c10051085bb5b68
Jewei Mak <jewei.mak@gmail.com>
2024-10-23 15:37:32 +0800

    API response in JSON

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,11 @@
 
 declare(strict_types=1);
 
+use app\Http\Middleware\ResponseWithJson;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,8 +15,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->api(prepend: [
+            ResponseWithJson::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->shouldRenderJsonWhen(
+            fn (Request $request) => $request->is('api/*') || $request->wantsJson()
+        );
     })->create();
